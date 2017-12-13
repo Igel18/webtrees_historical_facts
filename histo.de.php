460e913ccb9f80cfd8dd131a6e64e48bd21300f8
Igel18 <dominikprudlo@Dominiks-Pro.fritz.box>
2017-12-13 19:55:41 +0100

    Fixed formation & added events

--- histo.de.php
+++ histo.de.php
@@ -17,23 +17,30 @@ $histo[] = "1 EVEN\n2 TYPE Columbus entdeckt America\n2 DATE 1492\n2 NOTE Christ
 // Martin Luther
 $histo[] = "1 EVEN\n2 TYPE Martin Luther begründet die Reformation\n2 DATE 31 OKT 1517\n2 NOTE durch einen Brief an den Mainzer Erzbischof in dem er den Ablasshandel anprangert, s. https://de.wikipedia.org/wiki/Martin_Luther";
 
+// Dampfmaschine 
+$histo[] = "1 EVEN\n2 TYPE Dampfmaschine wurde konstruiert\n2 DATE 1712\n2 NOTE durch Thomas Newcomen , s. https://de.wikipedia.org/wiki/Dampfmaschine#Geschichte_der_Dampfmaschine";
+
 // Napoleon 
 $histo[] = "1 EVEN\n2 TYPE Napoleon wird Kaiser von Frankreis\n2 DATE 1804, s. https://de.wikipedia.org/wiki/Napoleon_Bonaparte";
 
+// Glühbirne 
+$histo[] = "1 EVEN\n2 TYPE Erfindung der Glühbirne \n2 DATE 27 JAN 1880\n2 NOTE durch die Patentanmeldung Thomas Edinsons, s. https://de.wikipedia.org/wiki/Gl%C3%BChlampe";
 
-// Cäsar 
+// Erfindung des Ottomobils 
+$histo[] = "1 EVEN\n2 TYPE Erfindung des Ottomobils \n2 DATE 1886\n2 NOTE durch die Patentanmeldung des deutschen Erfinders Carl Benz, s. https://de.wikipedia.org/wiki/Geschichte_des_Automobils";
 
-// Cleopatra 
+// Relativitätstheorie 
+$histo[] = "1 EVEN\n2 TYPE Veröffentlichung der Relativitätstheorie \n2 DATE 25 NOV 1915\n2 NOTE durch Albert Einstein, s. https://de.wikipedia.org/wiki/Allgemeine_Relativit%C3%A4tstheorie#Geschichte";
 
 // 1. Weltkrieg
 $histo[] = "1 EVEN\n2 TYPE Beginn Erster Weltkrieg\n2 DATE 28 JUL 1914\n2 NOTE durch die Kriegserklärung Österreich-Ungarns an Serbien, s. https://de.wikipedia.org/wiki/Erster_Weltkrieg";
-
 $histo[] = "1 EVEN\n2 TYPE Ende Erster Weltkrieg\n2 DATE 11 NOV 1918\n2 NOTE durch die Unterzeichnung eines Waffenstillstandsvertrages, s. https://de.wikipedia.org/wiki/Waffenstillstand_von_Compiègne_(1918)";
 
 // 2. Weltkrieg
-$histo[] = "1 EVEN\n2 TYPE Beginn Zweiter Weltkrieg\n2 DATE 01 SEP 1939\n2 NOTE durch den deutschen Überfall auf Polen"; 
+$histo[] = "1 EVEN\n2 TYPE Beginn Zweiter Weltkrieg\n2 DATE 01 SEP 1939\n2 NOTE durch den deutschen Überfall auf Polen, s. https://de.wikipedia.org/wiki/Zweiter_Weltkrieg"; 
+$histo[] = "1 EVEN\n2 TYPE Ende Zweiter Weltkrieg\n2 DATE 02 SEP 1945\n2 NOTE mit der Kapitulation Japans, s. https://de.wikipedia.org/wiki/Zweiter_Weltkrieg"; 
 
-// Bau der Mauer 
+// Bau der Berliner Mauer 
 $histo[] = "1 EVEN\n2 TYPE Bau der Berliner Mauer\n2 DATE 13 AUG 1961\n2 NOTE symbolischer \"Tag des Mauerbaus\", s. http://de.wikipedia.org/wiki/Berliner_Mauer";
 
 // Mondlandung
